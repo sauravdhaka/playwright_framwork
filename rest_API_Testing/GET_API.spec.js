ac421042d0892f8eff6e0b54159c963c69060437
sauravdhaka <sauravdhaka456@gmail.com>
2024-02-05 13:46:42 +0530

    TEST GET method and add aluure report

---
+++ rest_API_Testing/GET_API.spec.js
@@ -0,0 +1,44 @@
+import {test , expect} from "@playwright/test"
+
+
+test("Should be able to get all booking information", async ({request}) =>{
+    const response  =await request.get("/booking")
+    expect(response.status()).toBe(200)
+})
+
+
+test("Should be able to get all booking information by first name", async ({request}) =>{
+    const response  =await request.get("/booking?firstname=Jim")
+    expect(response.status()).toBe(200)
+
+})
+
+test("shoulld be able to get all bookings with firstname and last name", async({request})=>{
+    const response = await request.get("/booking?firstname=Jim&lastname=Brown")
+
+    expect(response.status()).toBe(200)
+    const result = await  response.json()
+    console.log(result);
+    expect(Object.keys(result).length).toBeGreaterThan(0);
+
+    
+})
+
+test("should be able to get booking with chekin and check out time",async({request})=>{
+    const response = await request.get("/booking?checkin=2014-03-13&checkout=2014-05-21")
+    expect(response.status()).toBe(200)
+    const result = await  response.json()
+})
+
+
+test("should be able to get booking with booking ID",async({request})=>{
+    const response = await request.get("/booking/175")
+    expect(response.status()).toBe(200)
+    const result = await response.json()
+    expect(Object.keys(result).length).toBeGreaterThan(0);
+    console.log(result)
+    expect(result.firstname).toBe("Josh")
+    expect(result.lastname).toBe("Allen")
+    expect(result.depositpaid).toBeTruthy()
+
+})
