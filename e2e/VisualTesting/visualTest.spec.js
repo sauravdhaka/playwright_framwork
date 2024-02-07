3f93b0544d7d805c6247d60961a139f8d3e1eb8d
sauravdhaka <sauravdhaka456@gmail.com>
2024-02-07 16:05:04 +0530

    added thersold to toHaveScreenshot

--- e2e/VisualTesting/visualTest.spec.js
+++ e2e/VisualTesting/visualTest.spec.js
@@ -3,27 +3,32 @@ import  {expect, test} from "@playwright/test"
 
 test("example test case",async({page})=>{
     await page.goto("https://magento.softwaretestingboard.com/customer/account/login/referer/aHR0cHM6Ly9tYWdlbnRvLnNvZnR3YXJldGVzdGluZ2JvYXJkLmNvbS8%2C/")
-    await expect(page).toHaveScreenshot()
+    await page.waitForTimeout(3000)
+    await expect(page).toHaveScreenshot({maxDiffPixels : 30000})
 })
 
 
 test("landing page visual test case",async({page})=>{
     await page.goto("https://magento.softwaretestingboard.com/")
-    await expect(page).toHaveScreenshot()
+    await page.waitForTimeout(3000)
+    await expect(page).toHaveScreenshot({maxDiffPixels : 30000})
 })
 
 
 test("Login page visual test case",async({page})=>{
     await page.goto("https://magento.softwaretestingboard.com/customer/account/login/referer/aHR0cHM6Ly9tYWdlbnRvLnNvZnR3YXJldGVzdGluZ2JvYXJkLmNvbS8%2C/")
-    await expect(page).toHaveScreenshot()
+    await page.waitForTimeout(3000)
+    await expect(page).toHaveScreenshot({maxDiffPixels : 30000})
 })
 
 test("signup page visual test case",async({page})=>{
     await page.goto("https://magento.softwaretestingboard.com/customer/account/create/")
-    await expect(page).toHaveScreenshot()
+    await page.waitForTimeout(3000)
+    await expect(page).toHaveScreenshot({maxDiffPixels : 30000})
 })
 
 test("signup page with partial part test case",async({page})=>{
     await page.goto("https://magento.softwaretestingboard.com/customer/account/create/")
-    await expect(page).toHaveScreenshot({mask : [page.locator('#form-validate > fieldset.fieldset.create.account')]})
+    await page.waitForTimeout(3000)
+    await expect(page).toHaveScreenshot({mask : [page.locator('#form-validate > fieldset.fieldset.create.account')] , maxDiffPixels : 30000})
 })
