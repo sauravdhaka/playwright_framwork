7f0793e545d55df8bd9d11476aa773c8dc61e8aa
sauravdhaka <sauravdhaka456@gmail.com>
2024-02-07 14:32:34 +0530

    visual test

--- e2e/VisualTesting/visualTest.spec.js
+++ e2e/VisualTesting/visualTest.spec.js
@@ -1,12 +1,18 @@
 import  {expect, test} from "@playwright/test"
 
 
-test("first visual test",async({page})=>{
+test("Landing page visual test",async({page})=>{
     await page.goto("https://magento.softwaretestingboard.com/")
+
     expect(await page.screenshot()).toMatchSnapshot('landing.png')
 })
 
-test("different screen shots",async({page})=>{
+test("Login page visual test",async({page})=>{
+    await page.goto("https://magento.softwaretestingboard.com/customer/account/login/referer/aHR0cHM6Ly9tYWdlbnRvLnNvZnR3YXJldGVzdGluZ2JvYXJkLmNvbS9jdXN0b21lci9hY2NvdW50L2NyZWF0ZS8%2C/")
+    expect(await page.screenshot()).toMatchSnapshot('login.png')
+})
+
+test("sign up page visual test",async({page})=>{
     await page.goto("https://magento.softwaretestingboard.com/customer/account/create/")
     expect(await page.screenshot()).toMatchSnapshot('register.png')
 })
